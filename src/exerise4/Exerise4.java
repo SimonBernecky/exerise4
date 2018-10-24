84a69c8493539186683c1781e92c094e4b6e887d
Bladetango6 <SCB13@students.ptcollege.edu>
2018-10-24 15:44:58 -0400

    Testing

---
+++ src/exerise4/Exerise4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exerise4;
+
+/**
+ *
+ * @author simbe
+ */
+public class Exerise4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+       
+    }
+    
+}
